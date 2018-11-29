4b8a196af85590777095aa815136753c785b6591
SatyamDeveloperModeOn <satyampandey.m@gmail.com>
2018-11-29 20:51:06 +0530

    Comment 404 page

--- app.js
+++ app.js
@@ -91,17 +91,17 @@ app.use('/', routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  var err = new Error('File Not Found');
-  err.status = 404;
-  next(err);
-});
-// error handler
-// define as the last app.use callback
-app.use(function (err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('404');
-});
+// app.use(function (req, res, next) {
+//   var err = new Error('File Not Found');
+//   err.status = 404;
+//   next(err);
+// });
+// // error handler
+// // define as the last app.use callback
+// app.use(function (err, req, res, next) {
+//   res.status(err.status || 500);
+//   res.render('404');
+// });
 
 
 
